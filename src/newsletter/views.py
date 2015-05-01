fe28adf2786760931ad51ad85127afde71b0fbf4
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-05-01 16:32:25 -0700

    Setup Email

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 
 from .forms import ContactForm, SignUpForm
@@ -36,13 +38,29 @@ def home(request):
 def contact(request):
 	form = ContactForm(request.POST or None)
 	if form.is_valid():
-		for key, value in form.cleaned_data.iteritems():
-			print key, value
-			#print form.cleaned_data.get(key)
-		# email = form.cleaned_data.get("email")
-		# message = form.cleaned_data.get("message")
-		# full_name = form.cleaned_data.get("full_name")
+		# for key, value in form.cleaned_data.iteritems():
+		# 	print key, value
+		# 	#print form.cleaned_data.get(key)
+		form_email = form.cleaned_data.get("email")
+		form_message = form.cleaned_data.get("message")
+		form_full_name = form.cleaned_data.get("full_name")
 		# print email, message, full_name
+		subject = 'Site contact form'
+		from_email = settings.EMAIL_HOST_USER
+		to_email = [from_email, 'youotheremail@email.com']
+		contact_message = "%s: %s via %s"%( 
+				form_full_name, 
+				form_message, 
+				form_email)
+		some_html_message = """
+		<h1>hello</h1>
+		"""
+		send_mail(subject, 
+				contact_message, 
+				from_email, 
+				to_email, 
+				html_message=some_html_message,
+				fail_silently=True)
 
 	context = {
 		"form": form,
--- src/trydjango18/settings.py
+++ src/trydjango18/settings.py
@@ -27,6 +27,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'yourgmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+''' 
+If using gmail, you will need to
+unlock Captcha to enable Django 
+to  send for you:
+https://accounts.google.com/displayunlockcaptcha
+'''
+
+
 
 # Application definition
 
