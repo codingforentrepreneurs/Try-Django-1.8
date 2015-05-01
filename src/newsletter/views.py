661647c6e50d381cd2bd8780c62d805b5a7c5946
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-05-01 14:17:51 -0700

    View & Template Context

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, "home.html", {})
+	title = 'Welcome'
+	if request.user.is_authenticated():
+		title = "My Title %s" %(request.user)
+	
+	#add a form
+	context = {
+		"title": title,
+	}
+	return render(request, "home.html", context)
