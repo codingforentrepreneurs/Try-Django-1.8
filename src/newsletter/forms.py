d63785c9906cc9cb0f7efc6e27cf2179e8132690
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-05-01 14:09:58 -0700

    Form Validation

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -7,4 +7,19 @@ class SignUpForm(forms.ModelForm):
 	class Meta:
 		model = SignUp
 		fields = ['full_name', 'email']
-		### exclude = ['full_name'] 
+		### exclude = ['full_name']
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		email_base, provider = email.split("@")
+		domain, extension = provider.split('.')
+		if not domain == 'USC':
+			raise forms.ValidationError("Please make sure you use your USC email.")
+		if not extension == "edu":
+			raise forms.ValidationError("Please use a valid .EDU email address")
+		return email
+
+	def clean_full_name(self):
+		full_name = self.cleaned_data.get('full_name')
+		#write validation code.
+		return full_name
