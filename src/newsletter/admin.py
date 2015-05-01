1ac79ed3c1c721d2e12bfea7f30508e218c3e357
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-04-30 18:22:42 -0700

    Admin

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SignUp
+
+class SignUpAdmin(admin.ModelAdmin):
+	list_display = ["__unicode__", "timestamp", "updated"]
+	class Meta:
+		model = SignUp
+
+
+
+admin.site.register(SignUp, SignUpAdmin)
