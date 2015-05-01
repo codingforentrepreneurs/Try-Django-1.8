a88a084fd396c67f168130565584495b59495e59
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-04-30 18:11:55 -0700

    Models

--- src/trydjango18/settings.py
+++ src/trydjango18/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'newsletter',
+    'newsletter',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'trydjango18.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates"), "/Users/jmitch/desktop/trydjango18/src/abctemplates"],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
