773144fabc00d85df492528985a86fccd82b2215
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-05-04 13:23:53 -0700

    Static files & Serve Static Files

--- src/trydjango18/settings.py
+++ src/trydjango18/settings.py
@@ -115,3 +115,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
+    
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static_in_pro", "our_static"),
+    #os.path.join(BASE_DIR, "static_in_env"),
+    #'/var/www/static/',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
+
+
+
+
+
+
+
+
--- src/trydjango18/urls.py
+++ src/trydjango18/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+
 
 urlpatterns = [
     # Examples:
@@ -9,3 +12,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
